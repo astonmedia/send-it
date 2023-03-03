ddd26457992b32140e8bfd24878f466c52e45128
astonmedia <astonmedia@hotmail.com>
2023-03-04 05:12:42 +1000

    finished project

---
+++ app/components/AddPost.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import toast from "react-hot-toast";
+
+const AddPost = () => {
+  const [title, setTitle] = useState("");
+  const [isDisabled, setIsDisabled] = useState(false);
+  const queryClient = useQueryClient();
+  let toastPostId: string;
+
+  // Create a Post
+  const { mutate } = useMutation(
+    async (title: string) => await axios.post("/api/posts/addPost", { title }),
+    {
+      onError: (error) => {
+        console.log(error);
+        if (error instanceof AxiosError) {
+          toast.error(error?.response?.data.message, { id: toastPostId });
+        }
+        setIsDisabled(false);
+      },
+      onSuccess: (data) => {
+        toast.success("Post has been made 🔥", { id: toastPostId });
+        queryClient.invalidateQueries(["posts"]);
+        setTitle(""), setIsDisabled(false), console.log(data);
+      },
+    }
+  );
+
+  const submitPost = async (e: React.FormEvent) => {
+    e.preventDefault();
+    toastPostId = toast.loading("Creating your post", { id: toastPostId });
+    setIsDisabled(true);
+    mutate(title);
+  };
+
+  return (
+    <form onSubmit={submitPost} className="bg-white my-8 p-8 rounded-md">
+      <div className="flex flex-col my-4">
+        <textarea
+          onChange={(e) => setTitle(e.target.value)}
+          name="title"
+          value={title}
+          placeholder="What's on your mind"
+          className="p-4 text-lg rounded-md my-2 bg-gray-200"
+        ></textarea>
+      </div>
+      <div className="flex items-center justify-between gap-2">
+        <p
+          className={`font-bold text-sm ${
+            title.length > 300 ? "text-red-700" : "text-gray-700"
+          }`}
+        >{`${title.length}/300`}</p>
+        <button
+          disabled={isDisabled}
+          type="submit"
+          className="text-sm bg-teal-600 text-white py-2 px-6 rounded-xl disabled:opacity-25"
+        >
+          Create a Post
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default AddPost;
